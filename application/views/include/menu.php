9b1d8f6d3f01ba74500b314d02d15aa24a202fe3
frnchscmarie <chesca.cortez@gmail.com>
2017-09-21 19:57:16 +0800

    Modified Menu and Images (Home/Log-in View)

--- application/views/include/menu.php
+++ application/views/include/menu.php
@@ -1,13 +1,20 @@
 <nav class="navbar navbar-default">
   <div class="container search">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#"><img src="<?php echo base_url('assets/images/logo.png'); ?>" />PETERNITY</a>
+    <div class="navbar-header menupad">
+      <a class="navbar-brand" href="#"><img src="<?php echo base_url('assets/images/fulllogo.png'); ?>" /></a>
     </div>
     <ul class="nav navbar-nav">
-      <li class=""><a class="u" href="#">HOME</a></li>
-      <li class=""><a class="u" href="#">PET CATALOGUE</a></li>
-      <li class=""><a class="u" href="#">COMMUNITY</a></li>
-      <li class="dropdown">
+      <li class="menupad"><a class="u" href="#">HOME</a></li>
+	  <li class="dropdown menupad">
+        <a class="u" class="dropdown-toggle" data-toggle="dropdown" href="#">PET CATALOGUE
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li class="menupad"><a href="#">RESCUED PETS</a></li>
+          <li class="menupad"><a href="#">PET FOR ADOPTION</a></li>
+        </ul>
+      </li>
+      <li class="menupad"><a class="u" href="#">COMMUNITY</a></li>
+      <li class="dropdown menupad">
         <a class="u" class="dropdown-toggle" data-toggle="dropdown" href="#">NEWS AND EVENTS
         <span class="caret"></span></a>
         <ul class="dropdown-menu">
@@ -15,10 +22,11 @@
           <li class="menupad"><a href="#">EVENTS</a></li>
         </ul>
       </li>
-      <li><a class="u" href="#">FAQ's</a></li>  
+      <li class="menupad"><a class="u" href="#">FAQ's</a></li>  
+      <li><a class="u" href="peternity/signup">SIGN UP</a></li>  
 	  
-	  <li><a class="u" href="peternity/signup"> SIGN-UP <a/></li>
-		
+	  
+	  
     </ul>
   </div>
 </nav>
