2f991d1967bec0ed33302079815001c2d30693a0
janegellado <janelynanngellado@gmail.com>
2017-09-29 23:09:23 +0800

    Adoption Form and Pet Catalogue(open for controller editing)

---
+++ application/views/peternity/adoptionform.php
@@ -0,0 +1,77 @@
+<div class=" container">
+  <div class="container">
+	<img src="<?php echo base_url('assets/images/pets.png'); ?>" class="img-responsive" style="width:100% ; height:200px" alt="logoform">
+  </div>
+   
+   <form class="form-horizontal" action="">
+    <div class="form-group petpic">
+	<div class="col-sm-12">
+		<div class="col-sm-12 ">
+			<img src="<?php echo base_url('assets/images/bugsy.jpg'); ?>" class="img-thumbnail" style="width: 200px; height:200px" alt="profilepic">
+		</div>
+     </div>
+     </div>
+     <div class="form-group">
+	<div class="col-sm-12 ">	
+	<div class="col-sm-12">	
+	 <label class="control-label col-sm-4 margin" for="oname">Owner Name: </label>
+      <div class="col-sm-4">
+        <input type="oname" class="form-control" id="oname" placeholder="Enter your name" name="oname">
+      </div>
+	  <br/>
+	  <br/>
+  </div>
+  
+  <div class="col-sm-12">	
+	 <label class="control-label col-sm-4 margin" for="contactno">Contact Number: </label>
+      <div class="col-sm-4">
+        <input type="contactno" class="form-control" id="contactno" placeholder="Enter contact number" name="">
+      </div>
+	  <br/>
+	  <br/>
+  </div>
+  
+  <div class="col-sm-12">	
+	 <label class="control-label col-sm-4 margin" for="address">Address: </label>
+      <div class="col-sm-4">
+        <input type="address" class="form-control" id="address" placeholder="Enter address" name="address">
+      </div>
+	  <br/>
+	  <br/>
+  </div>
+  
+	<div class="col-sm-12">	
+	  <div class="form-group">
+      <label class="control-label col-sm-4 margin" for="height">Pet Height & Weight: </label>
+      <div class="col-sm-2">
+        <input type="height" class="form-control" id="height" placeholder="Enter pet height" name="height">
+	  </div>
+      <div class="col-sm-2">
+        <input type="weight" class="form-control" id="weight" placeholder="Enter pet weight" name="weight">
+      </div>
+	  </div>
+	 </form><br/><br/>
+	 
+	 <div class="text-center">
+      <h4 class=" font text-center" >Pet Adopter's Oath <br /></h4>
+	  <p class="text-center ">I hereby swear to take good care of my pet. To feed them, play with them, and take care of their needs.<p>
+	  </div>
+	  
+	 <div class="form-group">        
+      <div class="col-sm-offset-2 col-sm-10">
+        <div class="checkbox text-center margin">
+          <label><input type="checkbox" name="remember">I agree to the adopter's oath.</label>
+        </div>
+      </div>
+    </div><br/><br/>
+	
+	<div class="form-group">        
+      <div class="col-sm-offset-2 col-sm-10 text-center margin">
+        <button type="submit" class="btn btn-default margin text-center "><span class="glyphicon glyphicon-heart">ADOPT</span></button>
+      </div>
+    </div>
+  </div>
+
+	  </div>
+	  </div>
+	
---
+++ application/views/peternity/catalogue.php
@@ -0,0 +1,90 @@
+<html>
+<div class="container text-center">    
+  <h1>PET CATALOGUE</h1><br>
+	
+	<div class="col-md-2">
+		<div>
+		<div class="menupad"><a href="rescuepet" style="font-size:20px;"><span class="glyphicon glyphicon-heart-empty">RESCUED PETS</span></a><br/><br />
+          <a href="petforadoption" style="font-size:20px;"><span class="glyphicon glyphicon-heart-empty">PET FOR ADOPTION</span></a>
+		  
+		   </div> </div>
+	</div>
+	
+	<div class="col-md-10">
+	
+	<div class="col-md-4">
+	<div class="thumbnail">
+    <div class="thumb">
+        <img src="<?php echo base_url('assets/images/r1.jpg'); ?>" alt="pet 1" style="width:100%"> 
+		 <div class="overlay">
+			<div class="text">Hello</div>
+		</div>
+		</div>
+		</div>
+	<div class="thumbnail">
+    <div class="thumb">
+        <img src="<?php echo base_url('assets/images/r5.jpg'); ?>"  alt="pet 2" style="width:100%">
+		<div class="overlay">
+			<div class="text">Hello</div>
+		</div>
+		</div>
+		</div>
+  <div class="thumbnail">
+  <div class="thumb">
+        <img src="<?php echo base_url('assets/images/r4.jpg'); ?>"  alt="pet 3" style="width:100%">
+    <div class="overlay">
+			<div class="text">Hello</div>
+		</div>
+		</div>
+		</div>
+	
+  </div>
+  <div class="col-md-4">
+  
+  <div class="thumbnail">
+    <div class="thumb">
+        <img src="<?php echo base_url('assets/images/cat.jpg'); ?>" alt="pet 4" style="width:100%">
+     <div class="overlay">
+			<div class="text">Hello</div>
+		</div>
+		</div>
+		</div>
+		
+		<div class="thumbnail">
+	 <div class="thumb">
+        <img src="<?php echo base_url('assets/images/hh.jpg'); ?>"  alt="pet 5" style="width:100%">
+      <div class="overlay">
+			<div class="text">Hello</div>
+		</div>
+		</div>
+		</div>
+  </div>
+  
+
+  
+  
+  <div class="col-md-4">
+  <div class="thumbnail">
+    <div class="thumb">
+        <img src="<?php echo base_url('assets/images/bunny.jpg'); ?>"  alt="pet 6" style="width:100%">
+      <div class="overlay">
+			<div class="text">Hello</div>
+		</div>
+		</div>
+		</div>
+  
+  <div class="thumbnail">
+    <div class="thumb">
+        <img src="<?php echo base_url('assets/images/r7.jpg'); ?>"  alt="pet 7" style="width:100%">
+        
+     <div class="overlay">
+			<div class="text">Hello</div>
+		</div>
+		</div>
+  </div>
+  
+  
+</div>
+ </div>
+ </div>
+</html> 
