48e81d9e9463b5de40f0252ea507f55c00c4fe92
unknown <alexiaarmando04@gmail.com>
2017-09-30 13:11:38 +0800

    Updated sql and controllers.

--- application/views/include/menu-faqs.php
+++ application/views/include/menu-faqs.php
@@ -5,25 +5,25 @@
     </div>
     <ul class="nav navbar-nav">
       <li class="menupad"><a class="u" href="index">HOME</a></li>
-	  <li class="menupad"><a class="u" href="petcatalogue">PET CATALOGUE</a></li>
+	  <li class="menupad"><a class="u" href="<?php echo base_url('peternity/petcatalogue')?>">PET CATALOGUE</a></li>
      <li class="dropdown menupad">
         <a class="u" class="dropdown-toggle" data-toggle="dropdown" href="">COMMUNITY
         <span class="caret"></span></a>
         <ul class="dropdown-menu">
-          <li class="menupad"><a href="stories">SUCCESS STORIES</a></li>
-          <li class="menupad"><a href="discussion">DICUSSIONS</a></li>
+          <li class="menupad"><a href="<?php echo base_url('peternity/stories')?>">SUCCESS STORIES</a></li>
+          <li class="menupad"><a href="<?php echo base_url('peternity/discussion')?>">DICUSSIONS</a></li>
         </ul>
       </li>
       <li class="dropdown menupad">
         <a class="u" class="dropdown-toggle" data-toggle="dropdown" href="">NEWS AND EVENTS
         <span class="caret"></span></a>
         <ul class="dropdown-menu">
-          <li class="menupad"><a href="news">NEWS</a></li>
+          <li class="menupad"><a href="<?php echo base_url('peternity/news')?>">NEWS</a></li>
           <li class="menupad"><a href="">UPCOMING EVENTS</a></li>
         </ul>
       </li>
-      <li class="menupad"><a class="u" href="">FAQ's</a></li>  
-      <li><a class="u" href="signup">SIGN UP</a></li>  
+      <li class="menupad"><a class="u" href="<?php echo base_url('peternity/faqs')?>">FAQ's</a></li>  
+      <li><a class="u" href="<?php echo base_url('peternity/signup')?>">SIGN UP</a></li>  
 	  
 	  
 	  
