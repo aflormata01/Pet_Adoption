0c9727dadd9eda6b9e216fe749f7d8579d0f2b8f
frnchscmarie <chesca.cortez@gmail.com>
2017-10-06 18:16:03 +0800

    Modified Home Page

--- application/views/include/menu.php
+++ application/views/include/menu.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-default">
-  <div class="container search">
+  <div class="container">
     <div class="navbar-header menupad">
-      <a class="navbar-brand" href=""><img src="<?php echo base_url('assets/images/fulllogo.png'); ?>" /></a>
+      <a class="navbar-brand" href=""><img src="<?php echo base_url('assets/images/logo.png'); ?>" /></a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="menupad"><a class="u" href="<?php echo base_url('peternity')?>">HOME</a></li>
+      <li class="menupad "><a class="u" href="<?php echo base_url('peternity')?>">HOME</a></li>
 	  <li class="menupad"><a class="u" href="<?php echo base_url('peternity/petcatalogue')?>">PET CATALOGUE</a></li>
-	  <li class="dropdown menupad">
+	  <li class="dropdown menupad ">
         <a class="u" class="dropdown-toggle" data-toggle="dropdown" href="#">COMMUNITY
         <span class="caret"></span></a>
         <ul class="dropdown-menu">
@@ -22,8 +22,7 @@
           <li class="menupad"><a href="<?php echo base_url('peternity/upcomingevents')?>">UPCOMING EVENTS</a></li>
         </ul>
       </li>
-      <li class="menupad"><a class="u" href="<?php echo base_url('peternity/faqs')?>">FAQ's</a></li>  
-      <li><a class="u" href="<?php echo base_url('peternity/signup')?>">SIGN UP</a></li>  
+      <li class="menupad"><a class="u" href="<?php echo base_url('peternity/faqs')?>">FAQ's</a></li>   
 	  
 	  
 	  
