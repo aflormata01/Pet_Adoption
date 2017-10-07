72b15931f961bfecfae26076193453ad193fbfc8
janegellado <janelynanngellado@gmail.com>
2017-10-07 12:06:58 +0800

    Modified PetCatalogue, RescuedPets, PetforAdoption

--- application/views/peternity/catalogue.php
+++ application/views/peternity/catalogue.php
@@ -1,90 +1,99 @@
-<html>
 <div class="container text-center">    
-  <h1>PET CATALOGUE</h1><br>
-	
-	<div class="col-md-2">
-		<div>
-		<div class="menupad"><a href="rescuepet" style="font-size:20px;"><span class="glyphicon glyphicon-heart-empty">RESCUED PETS</span></a><br/><br />
-          <a href="petforadoption" style="font-size:20px;"><span class="glyphicon glyphicon-heart-empty">PET FOR ADOPTION</span></a>
-		  
-		   </div> </div>
+  <h1>PET CATALOGUE</h1><br/>
+  
+	<div class="col-md-12">
+	 <p>
+		<a href="petcatalogue" style="text-decoration:none"><button type="button" class="btn  btn-lg button1">Pet Catalogue</a></button>&nbsp;&nbsp;&nbsp;&nbsp;
+		<a href="rescuepet" style="text-decoration:none"><button type="button" class="btn   btn-lg button1">Rescued Pets</a></button>&nbsp;&nbsp;&nbsp;&nbsp;
+		<a href="petforadoption" style="text-decoration:none"><button type="button" class="btn  btn-lg button1">Pets for Adoption</a></button>&nbsp;&nbsp;&nbsp;&nbsp;
+	</p>
+	<br/>
 	</div>
+	<div class="col-md-12">
 	
-	<div class="col-md-10">
 	
-	<div class="col-md-4">
-	<div class="thumbnail">
+  
+  <div class="col-md-4">
     <div class="thumb">
-        <img src="<?php echo base_url('assets/images/r1.jpg'); ?>" alt="pet 1" style="width:100%"> 
+        <img src="<?php echo base_url('assets/images/r1.jpg'); ?>" alt="pet 1" class="img-circle image img-responsive"> 
 		 <div class="overlay">
 			<div class="text">Hello</div>
 		</div>
 		</div>
-		</div>
-	<div class="thumbnail">
+		 <br/>
+	
     <div class="thumb">
-        <img src="<?php echo base_url('assets/images/r5.jpg'); ?>"  alt="pet 2" style="width:100%">
+        <img src="<?php echo base_url('assets/images/r5.jpg'); ?>"  alt="pet 2" class="img-circle image img-responsive">
 		<div class="overlay">
 			<div class="text">Hello</div>
 		</div>
-		</div>
-		</div>
-  <div class="thumbnail">
-  <div class="thumb">
-        <img src="<?php echo base_url('assets/images/r4.jpg'); ?>"  alt="pet 3" style="width:100%">
+	
+		</div>	
+		 <br/>
+<div class="thumb">
+        <img src="<?php echo base_url('assets/images/r4.jpg'); ?>"  alt="pet 3" class="img-circle image img-responsive">
     <div class="overlay">
 			<div class="text">Hello</div>
+		
 		</div>
-		</div>
-		</div>
-	
+		</div>	
+ <br/>		
   </div>
-  <div class="col-md-4">
   
-  <div class="thumbnail">
+<div class="col-md-4">
+  <div class="thumb">
+        <img src="<?php echo base_url('assets/images/r4.jpg'); ?>"  alt="pet 3" class="img-circle image img-responsive">
+    <div class="overlay">
+			<div class="text">Hello</div>
+		
+		</div>
+		</div>
+ <br/>
+  
+
     <div class="thumb">
-        <img src="<?php echo base_url('assets/images/cat.jpg'); ?>" alt="pet 4" style="width:100%">
+        <img src="<?php echo base_url('assets/images/cat.jpg'); ?>" alt="pet 4" class="img-circle image img-responsive">
      <div class="overlay">
 			<div class="text">Hello</div>
 		</div>
 		</div>
+		 <br/>
+	<div class="thumb">
+        <img src="<?php echo base_url('assets/images/hh.jpg'); ?>"  alt="pet 5" class="img-circle image img-responsive">
+      <div class="overlay">
+			<div class="text">Hello</div>
+		</div>
+		</div>
+		 <br/>
+ </div>
+ 
+  
+		
+	<div class="col-md-4">
+	 <div class="thumb">
+        <img src="<?php echo base_url('assets/images/hh.jpg'); ?>"  alt="pet 5" class="img-circle image img-responsive">
+      <div class="overlay">
+			<div class="text">Hello</div>
+		</div>
+		</div>
+   <br/>
+    <div class="thumb">
+        <img src="<?php echo base_url('assets/images/bunny.jpg'); ?>"  alt="pet 6" class="img-circle image img-responsive">
+      <div class="overlay">
+			<div class="text">Hello</div>
 		</div>
 		
-		<div class="thumbnail">
-	 <div class="thumb">
-        <img src="<?php echo base_url('assets/images/hh.jpg'); ?>"  alt="pet 5" style="width:100%">
-      <div class="overlay">
-			<div class="text">Hello</div>
 		</div>
-		</div>
-		</div>
-  </div>
-  
-
-  
-  
-  <div class="col-md-4">
-  <div class="thumbnail">
-    <div class="thumb">
-        <img src="<?php echo base_url('assets/images/bunny.jpg'); ?>"  alt="pet 6" style="width:100%">
-      <div class="overlay">
-			<div class="text">Hello</div>
-		</div>
-		</div>
-		</div>
-  
-  <div class="thumbnail">
-    <div class="thumb">
-        <img src="<?php echo base_url('assets/images/r7.jpg'); ?>"  alt="pet 7" style="width:100%">
+		 <br/>
+		<div class="thumb">
+        <img src="<?php echo base_url('assets/images/r7.jpg'); ?>"  alt="pet 7" class="img-circle image img-responsive">
         
      <div class="overlay">
 			<div class="text">Hello</div>
 		</div>
 		</div>
-  </div>
-  
-  
-</div>
- </div>
- </div>
-</html> 
+		 <br/>
+		
+		</div>
+		</div>
+		</div>
