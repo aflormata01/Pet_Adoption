74f58c25159629b3b154ec7ed4e6184717bbe9e3
frnchscmarie <chesca.cortez@gmail.com>
2017-10-16 04:29:18 +0800

    Change password form (views)

--- application/views/peternity_user/settings.php
+++ application/views/peternity_user/settings.php
@@ -73,7 +73,7 @@
 	<div class="form-group">
       <label class="control-label col-sm-2" for="username">Password:</label>
       <div class="col-sm-4">
-        <a href="peternity/changepass">Change Password</a>
+        <a href="user/changepass" class="heading">Change Password</a>
       </div>
 	</div>
 	
