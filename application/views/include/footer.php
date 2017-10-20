bc7be53d4ec3dee5639534fe71e3703f7cec708b
frnchscmarie <chesca.cortez@gmail.com>
2017-10-21 02:46:55 +0800

    Modified home page

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -1,4 +1,8 @@
 <footer class="container text-left footer">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script type="text/javascript" src="http://arrow.scrolltotop.com/arrow84.js"></script>
+<noscript>Not seeing a <a href="http://www.scrolltotop.com/">Scroll to Top Button</a>? Go to our FAQ page for more info.</noscript>
+
 <hr>
   <p class="col-sm-9 sm">All Rights Reserved &copy 2017</p> 
   <p class="col-sm-1 text-right"><a class="sm" href="#">Contacts </a></p>
