a729e77bc039e23d8ebf0874494485afa4fb8eec
janegellado <janelynanngellado@gmail.com>
2017-10-21 12:19:17 +0800

    News User (trial)

--- application/views/peternity_user/news.php
+++ application/views/peternity_user/news.php
@@ -1,15 +1,15 @@
 
 <div class="container text-center">    
   <h2 class="heading">NEWS</h2><br>
-	
+	<div class="row">
+	<div class="column">
 <?php
 foreach($news as $d){ 
   echo'
-  <div class="newss">
-	<div class="col-md-4">
+  
     <div class="thumbnail">
       <a href="assets/images/bugsy.jpg">
-        <img src="assets/images/bugsy.jpg" alt="pics" style="width:100%">
+        <img src="assets/images/bugsy.jpg" alt="Lights" style="width:100%">
         <div class="caption">
           <h4 class="text-center heading">'.$d['title'].'</h4>
 		  <h6 class="text-center sm">By: Peternity PH</h6>
@@ -17,12 +17,53 @@ foreach($news as $d){
 		  <h6 class="text-center sm">"'.$d['body'].'"</h6>
         </div>
       </a>
-    </div>
-	</div>
-	</div>
-
-    ';
+    </div>';
 }
 ?>	
   
+</div>
+
+<div class="column">
+<?php
+foreach($news as $d){ 
+  echo'
+  
+    <div class="thumbnail">
+      <a href="assets/images/bugsy.jpg">
+        <img src="assets/images/bugsy.jpg" alt="Lights" style="width:100%">
+        <div class="caption">
+          <h4 class="text-center heading">'.$d['title'].'</h4>
+		  <h6 class="text-center sm">By: Peternity PH</h6>
+		  <h5 class="text-center sm">'.$d['date'].'</h5>
+		  <h6 class="text-center sm">"'.$d['body'].'"</h6>
+        </div>
+      </a>
+    </div>';
+}
+?>	
+  
+</div>
+
+<div class="column">
+<?php
+foreach($news as $d){ 
+  echo'
+  
+    <div class="thumbnail">
+      <a href="assets/images/bugsy.jpg">
+        <img src="assets/images/bugsy.jpg" alt="Lights" style="width:100%">
+        <div class="caption">
+          <h4 class="text-center heading">'.$d['title'].'</h4>
+		  <h6 class="text-center sm">By: Peternity PH</h6>
+		  <h5 class="text-center sm">'.$d['date'].'</h5>
+		  <h6 class="text-center sm">"'.$d['body'].'"</h6>
+        </div>
+      </a>
+    </div>';
+}
+?>	
+  
+</div>
+
+</div>
 </div>
