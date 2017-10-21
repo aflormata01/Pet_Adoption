1686b531c7c955d0746e62c123a467400c97505d
janegellado <janelynanngellado@gmail.com>
2017-10-21 12:14:12 +0800

    Modified Home, News trial

--- application/views/peternity/news.php
+++ application/views/peternity/news.php
@@ -2,7 +2,7 @@
 <div class="container text-center">    
   <h2 class="heading">NEWS</h2><br>
 	<div class="row">
-	<div class="col-md-4">
+	<div class="column">
 <?php
 foreach($news as $d){ 
   echo'
@@ -21,5 +21,49 @@ foreach($news as $d){
 }
 ?>	
   
+</div>
+
+<div class="column">
+<?php
+foreach($news as $d){ 
+  echo'
+  
+    <div class="thumbnail">
+      <a href="assets/images/bugsy.jpg">
+        <img src="assets/images/bugsy.jpg" alt="Lights" style="width:100%">
+        <div class="caption">
+          <h4 class="text-center heading">'.$d['title'].'</h4>
+		  <h6 class="text-center sm">By: Peternity PH</h6>
+		  <h5 class="text-center sm">'.$d['date'].'</h5>
+		  <h6 class="text-center sm">"'.$d['body'].'"</h6>
+        </div>
+      </a>
+    </div>';
+}
+?>	
+  
+</div>
+
+<div class="column">
+<?php
+foreach($news as $d){ 
+  echo'
+  
+    <div class="thumbnail">
+      <a href="assets/images/bugsy.jpg">
+        <img src="assets/images/bugsy.jpg" alt="Lights" style="width:100%">
+        <div class="caption">
+          <h4 class="text-center heading">'.$d['title'].'</h4>
+		  <h6 class="text-center sm">By: Peternity PH</h6>
+		  <h5 class="text-center sm">'.$d['date'].'</h5>
+		  <h6 class="text-center sm">"'.$d['body'].'"</h6>
+        </div>
+      </a>
+    </div>';
+}
+?>	
+  
+</div>
+
 </div>
 </div>
