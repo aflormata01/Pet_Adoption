295b072332a53d23ef8da5069ce0697318466dea
frnchscmarie <chesca.cortez@gmail.com>
2017-10-22 13:23:40 +0800

    Admin Views for news and faq

---
+++ application/views/peternity/adminnews.php
@@ -0,0 +1,22 @@
+<div class="fluid">
+<div class="text-center col-sm-6" style="margin-left: 490px;">
+<h1 class="heading">PETERNITY-NEWS</h1>
+<button class="none heading "><span><i class="fa fa-plus-circle"></i></span> Add News</button>
+</div>
+
+<div class="text-center col-sm-8" style="margin-left: 365px;">
+
+        <div class="panel-body" style="margin-left: 50px;">
+			    <div class="col-sm-3 text-center">
+				
+				<button class="none heading sm"><a href="" class="heading sm"><span><i class="fa fa-edit"></i></span> Edit</a></button> |
+				<button class="none heading sm"><a href="" class="heading sm"><span><i class="fa fa-trash-o"></i></span> Delete</a></button>
+    </div>
+
+
+
+
+</div>
+
+
+</div>
