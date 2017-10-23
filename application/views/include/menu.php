bb731510b42c524ac9db3df2de15751a3691b5eb
janegellado <janelynanngellado@gmail.com>
2017-10-23 10:28:15 +0800

    Steps, palette

--- application/views/include/menu.php
+++ application/views/include/menu.php
@@ -1,45 +1,34 @@
-  <style>
-
+ <style>
 .dropdown {
     position: relative;
     display: inline-block;
 	background-color: #ffcab2;
 }
-
 .dropdown-content {
     display: none;
     position: absolute;
     background-color: #ffcab2;
     z-index: 1;
 }
-
 .dropdown-content a {
     padding: 5px 2px 5px 2px;
     text-decoration: none;
     display: block;
 	background-color: #ffcab2;
 }
-
 .dropdown-content a:hover {
 	text-decoration: none;
 	background-color: #ccffea;
 	
 }
-
 .dropdown:hover .dropdown-content {
     display: block;
-}
-
-.svg{
-	width:100px;
-	height:100px;
-	margin-top: -35px;
 }
 </style>
 <nav class="navbar">
   <div class="container">
     <div class="navbar-header menupad">
-      <a class="navbar-brand" href="<?php echo base_url('user')?>""><img src="<?php echo base_url('assets/svg/peternity_1.svg'); ?>" class="svg"/></a>
+      <a class="navbar-brand" href="<?php echo base_url('user')?>""><img src="<?php echo base_url('assets/images/logo.png'); ?>" /></a>
     </div>
     <ul class="nav navbar-nav show-on-hover">
       <li class="menupad "><a class="sm" href="<?php echo base_url('user')?>">HOME</a></li>
@@ -60,8 +49,8 @@
       </li>
       <li class="menupad"><a class="sm" href="<?php echo base_url('peternity/faqs')?>">FAQ's</a></li>   
 	  
-		<li class="menupad"><a class="sm" href="<?php echo base_url('peternity/steps')?>">ADOPT A PET!</a></li>   
-		<li class="menupad"><a class="sm" href="<?php echo base_url('login_admin')?>">ADMIN</a></li>   
+		<li class="menupad"><a class="sm" href="<?php echo base_url('peternity/signup')?>" style="text-decoration:none" data-toggle="modal" data-target="#myModal">ADOPT A PET!</a></li>   
+		
 	
 	  
 	  
